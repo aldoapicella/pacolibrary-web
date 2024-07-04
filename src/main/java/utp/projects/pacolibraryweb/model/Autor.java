96e09d21b8edf9e9ee4cb4a69ce0fe015c02bab1
aldoapicella <aldoapicella@outlook.com>
2024-07-03 19:40:13 -0500

    refactor: Add Autor class to model package

---
+++ src/main/java/utp/projects/pacolibraryweb/model/Autor.java
@@ -0,0 +1,56 @@
+package utp.projects.pacolibraryweb.model;
+
+/**
+ * Represents an author in the library system.
+ */
+public class Autor {
+    private int id;
+    private String fullName;
+
+    /**
+     * Constructs an Autor object with the specified details.
+     *
+     * @param id
+     * @param fullName
+     */
+    public Autor(int id, String fullName) {
+        this.id = id;
+        this.fullName = fullName;
+    }
+
+    /**
+     * Returns the ID of the author.
+     *
+     * @return the author ID
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Sets the ID of the author.
+     *
+     * @param id the author ID to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * Returns the full name of the author.
+     *
+     * @return the full name
+     */
+    public String getFullName() {
+        return fullName;
+    }
+
+    /**
+     * Sets the full name of the author.
+     *
+     * @param fullName the full name to set
+     */
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+}
